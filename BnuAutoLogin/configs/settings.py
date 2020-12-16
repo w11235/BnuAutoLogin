f2a6dffa8633cae0b745cd5d942c0be64d2f645e
李超 <easy_318@qq.com>
2020-12-16 10:17:37 +0800

    更新项目目录,删除无用的测试代码

--- BnuAutoLogin/configs/settings.py
+++ BnuAutoLogin/configs/settings.py
@@ -2,16 +2,18 @@ from pathlib import Path
 import os
 
 # 根目录
+# root_path = Path(os.path.dirname())
 root_path = Path(os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.dirname((os.path.abspath(__file__)))))))
+    os.path.dirname((os.path.abspath(__file__))))))
 
 # ######项目类######
 # 项目目录
-project_path = root_path /"BnuAutoLogin"
-# 数据库目录
-DATA_DIR = project_path / "src"
+project_path = root_path / "BnuAutoLogin"
 # logs目录
 LOGS_DIR = project_path / "logs"
+# 静态文件目录
+STATIC_DIR = project_path / "static"
+
 
 # ######测试类######
 # 测试目录
@@ -21,4 +23,4 @@ TEST_DIR = project_path / "test"
 if __name__ == "__main__":
     print(root_path)
     print(project_path)
-    print(DATA_DIR)
+    print(TEST_DIR)
